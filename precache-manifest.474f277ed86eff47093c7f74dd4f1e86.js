190cd5546df16548fc6b8c412f79f34efac788d9
envick <envick1@mail.ru>
2020-07-19 14:38:06 +0300

    Updates

---
+++ precache-manifest.474f277ed86eff47093c7f74dd4f1e86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f59b75b0189161d2f86e8c0ea9696211",
+    "url": "/tile-game/index.html"
+  },
+  {
+    "revision": "75f4724bf5bc0568826f",
+    "url": "/tile-game/static/css/main.fb8c5e31.chunk.css"
+  },
+  {
+    "revision": "afd768e51e03138668b3",
+    "url": "/tile-game/static/js/2.e84ad2c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tile-game/static/js/2.e84ad2c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75f4724bf5bc0568826f",
+    "url": "/tile-game/static/js/main.7425a316.chunk.js"
+  },
+  {
+    "revision": "35a8c34fcfb08ee28e18",
+    "url": "/tile-game/static/js/runtime-main.e8d7a1af.js"
+  }
+]);
